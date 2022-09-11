58974177e961deadc75bfffa91c493c73679042d
Savchenko Alexander <sashulinator@gmail.com>
2022-09-11 22:25:54 +0400

    refactoring

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,61 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-console.log(process.env.PGHOST);
 
-@Controller()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { Pool } = require('pg');
+const pool = new Pool();
+pool.connect();
+
+@Controller('/api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('/graphic-1')
+  async getGraphs1() {
+    const source = await pool.query(
+      'SELECT DISTINCT source FROM "1_csv" WHERE source IS NOT NULL',
+    );
+    const credit_type = await pool.query(
+      'SELECT DISTINCT credit_type FROM "1_csv" WHERE credit_type IS NOT NULL',
+    );
+    return {
+      source: source.rows.map(({ source }) => source),
+      credit_type: credit_type.rows.map(({ credit_type }) => credit_type),
+    };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/graphic-2')
+  async getGraphs2() {
+    const tonality = await pool.query(
+      'SELECT DISTINCT tonality FROM "5_csv" WHERE tonality IS NOT NULL',
+    );
+    const source = await pool.query(
+      'SELECT DISTINCT source FROM "5_csv" WHERE source IS NOT NULL',
+    );
+    const credit_type = await pool.query(
+      'SELECT DISTINCT credit_type FROM "5_csv" WHERE credit_type IS NOT NULL',
+    );
+
+    return {
+      tonality: tonality.rows.map(({ tonality }) => tonality),
+      source: source.rows.map(({ source }) => source),
+      credit_type: credit_type.rows.map(({ credit_type }) => credit_type),
+    };
+  }
+
+  @Get('/graphic-3')
+  async getGraphs3() {
+    const tonality = await pool.query(
+      'SELECT DISTINCT tonality FROM "6_csv" WHERE tonality IS NOT NULL',
+    );
+    const source = await pool.query(
+      'SELECT DISTINCT source FROM "6_csv" WHERE source IS NOT NULL',
+    );
+    const credit_type = await pool.query(
+      'SELECT DISTINCT credit_type FROM "6_csv" WHERE credit_type IS NOT NULL',
+    );
+
+    return {
+      tonality: tonality.rows.map(({ tonality }) => tonality),
+      source: source.rows.map(({ source }) => source),
+      credit_type: credit_type.rows.map(({ credit_type }) => credit_type),
+    };
   }
 }
