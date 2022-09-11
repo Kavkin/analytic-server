fc6c1069a2fc7c98f67fae7c71a7ea4362c6d65c
Ilya <ilyagonnov@gmail.com>
2022-09-11 20:34:15 +0400

    add connected the database and added endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphsController } from './graphs/graphs.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, GraphsController],
   providers: [AppService],
 })
 export class AppModule {}
