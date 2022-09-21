da2badcc5505ca25e99d1ee8b045a6b0a317f895
Savchenko Alexander <sashulinator@gmail.com>
2022-09-21 10:50:10 +0400

    cool2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,13 +10,13 @@ export class AppController {
   @Get('/graphic-1')
   async getGraphs1() {
     const tonality = await pool.query(
-      'SELECT DISTINCT tonality FROM nlp."1_csv" WHERE tonality IS NOT NULL',
+      'SELECT DISTINCT tonality FROM nlp."v_1_csv" WHERE tonality IS NOT NULL',
     );
     const source = await pool.query(
-      'SELECT DISTINCT source FROM nlp."1_csv" WHERE source IS NOT NULL',
+      'SELECT DISTINCT source FROM nlp."v_1_csv" WHERE source IS NOT NULL',
     );
     const credit_type = await pool.query(
-      'SELECT DISTINCT credit_type FROM nlp."1_csv" WHERE credit_type IS NOT NULL',
+      'SELECT DISTINCT credit_type FROM nlp."v_1_csv" WHERE credit_type IS NOT NULL',
     );
     return {
       tonality: tonality.rows.map(({ tonality }) => tonality),
@@ -28,13 +28,13 @@ export class AppController {
   @Get('/graphic-2')
   async getGraphs2() {
     const tonality = await pool.query(
-      'SELECT DISTINCT tonality FROM nlp."5_csv" WHERE tonality IS NOT NULL',
+      'SELECT DISTINCT tonality FROM nlp."v_5_csv" WHERE tonality IS NOT NULL',
     );
     const source = await pool.query(
-      'SELECT DISTINCT source FROM nlp."5_csv" WHERE source IS NOT NULL',
+      'SELECT DISTINCT source FROM nlp."v_5_csv" WHERE source IS NOT NULL',
     );
     const credit_type = await pool.query(
-      'SELECT DISTINCT credit_type FROM nlp."5_csv" WHERE credit_type IS NOT NULL',
+      'SELECT DISTINCT credit_type FROM nlp."v_5_csv" WHERE credit_type IS NOT NULL',
     );
 
     return {
@@ -47,10 +47,10 @@ export class AppController {
   @Get('/graphic-3')
   async getGraphs3() {
     const source = await pool.query(
-      'SELECT DISTINCT source FROM nlp."6_csv" WHERE source IS NOT NULL',
+      'SELECT DISTINCT source FROM nlp."v_6_csv" WHERE source IS NOT NULL',
     );
     const credit_type = await pool.query(
-      'SELECT DISTINCT credit_type FROM nlp."6_csv" WHERE credit_type IS NOT NULL',
+      'SELECT DISTINCT credit_type FROM nlp."v_6_csv" WHERE credit_type IS NOT NULL',
     );
 
     return {
